a19f7dfdcf66c95100afda98646197acd8521bfe
Daniel Fischer <fische.daniel@gmail.com>
2017-02-26 11:56:43 -0500

    fix CORS issue

--- src/app/testService.ts
+++ src/app/testService.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class TestService{
   constructor(private http: Http){}
-  private baseUrl: string = 'http://localhost:8080';
+  private baseUrl: string = 'http://127.0.0.1:8080';
   getString(){
     let test$ = this.http.get(`${this.baseUrl}/greeting`, {headers: this.getHeaders()});
   test$.subscribe(result => {
@@ -15,7 +15,7 @@ export class TestService{
   private getHeaders(){
     let headers = new Headers();
     headers.append('Accept','application/json');
-    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
     return headers;
   }
 }
